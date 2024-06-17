d9dbb32409b06a60fb0bd14f879970c1d42aa807
trihai <damtrihaiz@gmail.com>
2024-06-17 16:41:23 +0700

    Responsive phần Navbar #1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,7 +25,7 @@ function load(selector, path) {
             }
         })
         .finally(() => {
-            window.dispatchEvent(new Event("template-loaded"));
+            window.dispatchEvent(new Event('template-loaded'));
         });
 }
 
@@ -36,13 +36,13 @@ function load(selector, path) {
 function isHidden(element) {
     if (!element) return true;
 
-    if (window.getComputedStyle(element).display === "none") {
+    if (window.getComputedStyle(element).display === 'none') {
         return true;
     }
 
     let parent = element.parentElement;
     while (parent) {
-        if (window.getComputedStyle(parent).display === "none") {
+        if (window.getComputedStyle(parent).display === 'none') {
             return true;
         }
         parent = parent.parentElement;
@@ -73,21 +73,21 @@ function debounce(func, timeout = 300) {
  * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
  */
 const calArrowPos = debounce(() => {
-    if (isHidden($(".js-dropdown-list"))) return;
+    if (isHidden($('.js-dropdown-list'))) return;
 
-    const items = $$(".js-dropdown-list > li");
+    const items = $$('.js-dropdown-list > li');
 
     items.forEach((item) => {
         const arrowPos = item.offsetLeft + item.offsetWidth / 2;
-        item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
+        item.style.setProperty('--arrow-left-pos', `${arrowPos}px`);
     });
 });
 
 // Tính toán lại vị trí arrow khi resize trình duyệt
-window.addEventListener("resize", calArrowPos);
+window.addEventListener('resize', calArrowPos);
 
 // Tính toán lại vị trí arrow sau khi tải template
-window.addEventListener("template-loaded", calArrowPos);
+window.addEventListener('template-loaded', calArrowPos);
 
 /**
  * Giữ active menu khi hover
@@ -97,12 +97,12 @@ window.addEventListener("template-loaded", calArrowPos);
  * 2. Thêm class "js-dropdown" vào class "dropdown" hiện tại
  *  nếu muốn reset lại item active khi ẩn menu
  */
-window.addEventListener("template-loaded", handleActiveMenu);
+window.addEventListener('template-loaded', handleActiveMenu);
 
 function handleActiveMenu() {
-    const dropdowns = $$(".js-dropdown");
-    const menus = $$(".js-menu-list");
-    const activeClass = "menu-column__item--active";
+    const dropdowns = $$('.js-dropdown');
+    const menus = $$('.js-menu-list');
+    const activeClass = 'menu-column__item--active';
 
     const removeActive = (menu) => {
         menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
@@ -130,5 +130,34 @@ function handleActiveMenu() {
 
     dropdowns.forEach((dropdown) => {
         dropdown.onmouseleave = () => init();
+    });
+}
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * <div id="box">Content show/hide</div>
+ */
+window.addEventListener('template-loaded', initJsToggle);
+
+function initJsToggle() {
+    $$('.js-toggle').forEach((button) => {
+        const target = button.getAttribute('toggle-target');
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains('hide');
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle('hide', !isHidden);
+                $(target).classList.toggle('show', isHidden);
+            });
+        };
     });
 }
